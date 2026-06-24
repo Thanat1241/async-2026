c08a3b85a0178b7b935d6d968c31a0618cdb6637
Thanat Nateyong <6720301003@cdti.ac.th>
2026-06-24 12:19:31 +0700

    <py> Fix03

--- Week1/up01_synchronous.py
+++ Week1/up01_synchronous.py
@@ -1,26 +1,26 @@
 from time import sleep, ctime, time
 
 def update_cup_number(customer_name):
-    print(f"{ctime()}: LCD: Processing for customer {customer_name}.")
+    print(f"{ctime()} | LCD: Processing for customer {customer_name}...")
     sleep(1.0)  # จำลองเวลาที่ใช้ในการอัปเดตหมายเลขถ้วยกาแฟ
-    print(f"{ctime()}: LCD: Done for customer {customer_name}.")
+    print(f"{ctime()} | LCD: Done for customer {customer_name}.")
     pass
 
 def make_coffee(customer_name):
-    print(f"{ctime()}: Making coffee for {customer_name}.")
+    print(f"{ctime()} | Making coffee for {customer_name}...")
     sleep (1.0)  # จำลองเวลาที่ใช้ในการทำกาแฟ
-    print(f"{ctime()}: Coffee Ready for {customer_name}.")
+    print(f"{ctime()} | Coffee Ready for {customer_name}!")
     pass
 
 def main():
-    print(f"{ctime()}: === Synchronous Coffee Machine ===")
+    print(f"{ctime()} | === Synchronous Coffee Machine ===")
     start = time()
 
     for customer_name in ["A", "B", "C"]:
         make_coffee(customer_name)
         update_cup_number(customer_name)
     
-    print(f"{ctime()}: Total time: {time() - start:.2f} seconds.")
+    print(f"{ctime()} | Total time: {time() - start:.2f} seconds.")
 
     pass
 
--- Week1/up04_asyncio.py
+++ Week1/up04_asyncio.py
@@ -5,30 +5,26 @@ import threading
 
 
 async def update_cup_number(customer_name):
-    print(f"{ctime()} [PID {os.getpid()} | {threading.current_thread().name}]: "
-          f"LCD: processing for customer {customer_name}.")
+    print(f"{ctime()} | LCD: processing for customer {customer_name}...")
     await asyncio.sleep(1.0)
-    print(f"{ctime()} [PID {os.getpid()} | {threading.current_thread().name}]: "
-          f"LCD: Done for customer {customer_name}.")
+    print(f"{ctime()} | LCD: Done for customer {customer_name}.")
     pass
 
 async def make_coffee(customer_name):
-    print(f"{ctime()} [PID {os.getpid()} | {threading.current_thread().name}]: "
-          f"Making coffee for {customer_name}.")
+    print(f"{ctime()} | Making coffee for {customer_name}...")
     await asyncio.sleep(1.0)
-    print(f"{ctime()} [PID {os.getpid()} | {threading.current_thread().name}]: "
-          f"Coffee Ready for {customer_name}.")
+    print(f"{ctime()} | Coffee Ready for {customer_name}!")
     await update_cup_number(customer_name)
 
 
 async def main():
-    print(f"{ctime()}: === Asyncio Coffee Machine ===")
+    print(f"{ctime()} | === Asyncio Coffee Machine ===")
     start = time()
 
     tasks = [make_coffee(customer_name) for customer_name in ["A", "B", "C"]]
     await asyncio.gather(*tasks)
 
-    print(f"{ctime()}: Total time: {time() - start:.2f} seconds.")
+    print(f"{ctime()} | Total time: {time() - start:.2f} seconds.")
     pass
 
 if __name__ == "__main__":
